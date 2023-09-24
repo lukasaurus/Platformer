687478a688b2f946684e48fc9fb31612998e0df0
lukasaurus <lukasaurus@gmail.com>
2023-09-24 19:59:48 +1300

    Enemy Basic Done

---
+++ Scenes/enemy_eagle.gd
@@ -0,0 +1,19 @@
+extends Area2D
+@export var flip_time = 1
+var direction = 1
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$Timer.wait_time = flip_time
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	translate(Vector2.RIGHT * direction)
+	$AnimatedSprite2D.flip_h = direction > 0
+	
+
+
+
+func _on_timer_timeout():
+	direction *= -1 
+	
